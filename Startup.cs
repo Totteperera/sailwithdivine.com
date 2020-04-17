dedc61c13787a1e77ff376613993c78488cb2cda
TottePerera <totteperera@gmail.com>
2020-04-17 15:43:41 +0200

    use forwarded headers

--- Startup.cs
+++ Startup.cs
@@ -36,14 +36,13 @@ namespace sailwithdivine.com
             }
             else
             {
-                app.UseForwardedHeaders(new ForwardedHeadersOptions {
-                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-                });
-                
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseForwardedHeaders(new ForwardedHeadersOptions {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
