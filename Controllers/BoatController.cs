88a2f35ca7fc98b79fe4739bd002fb0b401048a4
TottePerera <totteperera@gmail.com>
2020-09-12 16:11:16 +0200

    boat interior

--- Controllers/BoatController.cs
+++ Controllers/BoatController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using sailwithdivine.com.Models.Boat;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +11,22 @@ namespace sailwithdivine.com.Controllers
 {
     public class BoatController : Controller
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public BoatController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
         public IActionResult Index()
         {
-            return View();
+            var model = new BoatViewModel
+            {
+                ImageUrls = _webHostEnvironment.WebRootFileProvider.GetDirectoryContents("Content/Boat/Interior/")
+                .Select(x => $"Content/Boat/Interior/{x.Name}").ToList()
+            };
+
+            
+            return View(model);
         }
     }
 }
